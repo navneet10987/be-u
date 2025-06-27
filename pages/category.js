25ec0e9a22f01949e25dd235afd1d75be08881c4
Navneet Ohri <navneet10987@gmail.com>
2025-06-28 01:15:39 +0530

    Fix: Add default export to category and product pages

--- pages/category.js
+++ pages/category.js
@@ -1,1 +1,7 @@
-// Placeholder content for deployment
+// pages/category.js
+export default function CategoryPage() {
+  return <div>This is the Category Page</div>;
+}
+export default function PageName() {
+  return <div>Page content</div>;
+}
--- pages/product.js
+++ pages/product.js
@@ -1,1 +1,7 @@
-// Placeholder content for deployment
+// pages/product.js
+export default function ProductPage() {
+  return <div>This is the Product Page</div>;
+}
+export default function PageName() {
+  return <div>Page content</div>;
+}
